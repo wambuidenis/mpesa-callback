dd3f645eafddc9d2fd5b5a571b4920d4df94fc37
simons22d <denniskiruku@gmail.com>
2020-07-03 13:45:51 +0300

    reversal url

--- app.py
+++ app.py
@@ -34,6 +34,30 @@ def listenb2c():
     return jsonify({'message': message}), 200
 
 
+
+@app.route("/mpesa/reversals",methods=["POST"])
+def reversals():
+    # save the data
+    request_data = request.data
+    logging.info("Callback url called with the data ", request_data)
+    decoded = request_data.decode()
+    # Perform your processing here e.g. print it out...
+    requests.post(f"http://{ip}:9000/payment/status/reversal", json=decoded)
+
+    # here we are going to emit and event for the key
+    # Prepare the response, assuming no errors have occurred. Any response
+    # other than a 0 (zero) for the 'ResultCode' during Validation only means
+    # an error occurred and the transaction is cancelled
+    message = {
+        "ResultCode": 0,
+        "ResultDesc": "The service was accepted successfully",
+        "ThirdPartyTransID": secrets.token_hex()
+    }
+    # Send the response back to the server
+    return jsonify({'message': message}), 200
+
+
+
 # Change this part to reflect the API you are testing
 @app.route('/mpesa/b2b/v1')
 def listenb2b():
