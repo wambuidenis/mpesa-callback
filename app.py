2a277595baf2d3769e6550e6646bb75086f489d8
simons22d <denniskiruku@gmail.com>
2020-07-08 16:22:15 +0300

    refactor  > payments

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import logging
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from .payment import Mpesa, MpesaSchema, Payments,PaymentSchema
+from datetime import datetime
 
 logging.basicConfig(filename="mpesa.log", filemode="a", format='%(name)s - %(levelname)s - %(message)s')
 
@@ -21,6 +21,47 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 ip = "68.183.89.127"
+
+
+# paymnets schema
+
+class Payments(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    def __init__(self, body):
+        self.body = body
+
+
+class PaymentSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "message")
+
+
+class Mpesa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=True)
+    receipt_number = db.Column(db.String(255), nullable=True)
+    transaction_date = db.Column(db.String(255), nullable=True)
+    phone_number = db.Column(db.Integer, nullable=True)
+    checkout_request_id = db.Column(db.String(255), nullable=True)
+    merchant_request_id = db.Column(db.String(255), nullable=True)
+    result_code = db.Column(db.Integer, nullable=False)
+    result_desc = db.Column(db.Text, nullable=True)
+    date_added = db.Column(db.DateTime(), default=datetime.now)
+    local_transactional_key = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, MerchantRequestID, CheckoutRequestID, ResultCode, ResultDesc):
+        self.merchant_request_id = MerchantRequestID
+        self.checkout_request_id = CheckoutRequestID
+        self.result_code = ResultCode
+        self.result_desc = ResultDesc
+
+
+class MpesaSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "amount", "receipt_number", "transaction_date", "phone_number", "checkout_request_id",
+                  "merchant_request_id", "result_code", "result_desc", "date_added", "local_transactional_key")
 
 
 # You may create a separate URL for every endpoint you need
