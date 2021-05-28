361d767e09e58f5a95a43aa149e2eb03fc713635
simons22d <denniskiruku@gmail.com>
2021-05-28 16:40:47 +0300

    who knows

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fuprox import app
+import eventlet.wsgi
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=65123)
+    # app.run("0.0.0.0", port=65123)
+    eventlet.wsgi.server(eventlet.listen(('', 65123)), app)
